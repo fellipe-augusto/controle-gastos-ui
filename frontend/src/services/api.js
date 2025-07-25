a80bd3085b0786d49947a86f8b8310ec0758a1c7
Fellipe Augusto Reis Evangelista <34405426+fellipe-augusto@users.noreply.github.com>
2025-07-25 10:10:19 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-// A URL base agora virá de uma variável de ambiente VITE_API_BASE_URL
-// Se a variável não existir (em desenvolvimento local), ele usa a URL do localhost.
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
-});
+const apiUrl = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
 
 // ===================================================================
 // LINHA DE DEBUG: Isto vai nos mostrar a URL no console do navegador
 console.log("A API está configurada para usar a seguinte URL:", apiUrl);
 // ===================================================================
+
+const api = axios.create({
+  baseURL: apiUrl,
+});
 
 export default api;
